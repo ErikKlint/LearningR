39ca8e376a2e4520fe2c8250fb084b8cddb6a4ed
Erik Klint <svedberg.e+git@gmail.com>
2023-01-16 16:01:35 +0100

    add new R/ older, including README and learning script

---
+++ R/learning.R
@@ -0,0 +1,21 @@
+# R basics ----------------------------------------------------------------
+
+weight_kilos <- 100
+weight_kilos
+str(weight_kilos)
+head(weight_kilos)
+
+2 + 2
+
+colnames(airquality)
+
+
+#  Packages ---------------------------------------------------------------
+
+library(tidyverse)
+
+
+
+
+
+
