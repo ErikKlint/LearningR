a2041e9a302e7373aa1b058611810fb70ba45e51
Erik Klint <svedberg.e+git@gmail.com>
2023-01-17 10:54:15 +0100

    revisions for exercise in wrangling

--- R/learning.R
+++ R/learning.R
@@ -1,23 +1,19 @@
-
-# Here is an example of an conflict
-
-
-# R basics ----------------------------------------------------------------
-
-weight_kilos <- 100
-weight_kilos
-str(weight_kilos)
-head(weight_kilos)
-
-2 + 2
-
-colnames(airquality)
-
+# Chapter 7: Data management and wrangling
 
 #  Packages ---------------------------------------------------------------
-
 library(tidyverse)
-# added this description to testing out Git
+library(NHANES)
+
+
+
+# Initial analyze ---------------------------------------------------------
+
+# Briefly glimpse contents of dataset
+glimpse(NHANES)
+
+
+
+
 
 
 
