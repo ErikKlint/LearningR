113ee5208b7e7d739e24d16bb3737938f5a66eed
Erik Klint <svedberg.e+git@gmail.com>
2023-01-17 11:07:36 +0100

    updated initial data analyzing

--- R/learning.R
+++ R/learning.R
@@ -5,17 +5,30 @@ library(tidyverse)
 library(NHANES)
 
 
-
 # Initial analyze ---------------------------------------------------------
 
 # Briefly glimpse contents of dataset
 glimpse(NHANES)
+str(NHANES)
+colnames(NHANES)
 
+# Select one column by its name, without quotes
+select(NHANES, Age)
 
+# Select two or more columns by name, without quotes
+select(NHANES, Age, Weight, BMI)
 
+# To *exclude* a column, use minus (-)
+select(NHANES, -HeadCirc, -Gender)
 
+# All columns starting with letters "BP" (blood pressure)
+select(NHANES, starts_with("BP"))
 
+# All columns ending in letters "Day"
+select(NHANES, ends_with("Day"))
 
+# All columns containing letters "Age"
+select(NHANES, contains("Age"))
 
 
 
